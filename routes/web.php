870e3f8ae9d23b3b504e45c86d488b4e3905524f
Aanbayusaputra <aanbayusaputra06@gmail.com>
2023-05-25 00:04:21 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +19,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
-
-Route::get('/user', [UserController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
 Route::get('/add', [UserController::class, 'add']);
 Route::get('/edit', [UserController::class, 'edit']);
+Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+Route::get('/role', [RoleController::class, 'index'])->name('role.index');
