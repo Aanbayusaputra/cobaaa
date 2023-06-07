dd6a5420c92b823940bb0bc9c9d2880b41ec6d11
Aanbayusaputra <aanbayusaputra06@gmail.com>
2023-06-07 10:05:40 +0700

    task27 API

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -19,7 +19,7 @@
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="#!">My Store</a>
+            <a class="navbar-brand" href="{{ route('landing') }}">My Store</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
